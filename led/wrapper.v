beb0bf7f2dba3020be884db34090714094c1fd53
Adam Sandberg Ericsson <adam@sandbergericsson.se>
2021-04-05 19:32:00 +0100

    connects leds to switches

--- led/wrapper.v
+++ led/wrapper.v
@@ -1,13 +1,18 @@
 module Top (
   input clk,
-  output [15:0] led
+  output [15:0] led,
+  input [15:0] sw
 );
 
 wire rst;
 
-mkTop main(.CLK(clk), .RST_N(rst), .leds_leds(led));
+mkTop main(
+  .CLK(clk),
+  .RST_N(rst),
+  .leds_leds(led),
+  .switches_switches_1(sw)
+);
 
-assign rst = 0;
+assign rst = 1;
 
 endmodule
-
