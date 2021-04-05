f05e5ce7584839acefffdd4f4c73c32ec0b2eb00
Adam Sandberg Ericsson <adam@sandbergericsson.se>
2021-04-05 19:00:06 +0100

    nix stuff

---
+++ shell.nix
@@ -0,0 +1,27 @@
+{ sources ? import nix/sources.nix
+, pkgs ? import sources.nixpkgs {}
+}:
+
+let
+  hs = pkgs.haskellPackages.ghcWithPackages (p: [ p.regex-compat p.syb p.old-time p.split ]);
+in
+
+pkgs.mkShell {
+  buildInputs = [
+    hs
+    pkgs.haskellPackages.cabal-install
+    pkgs.haskellPackages.pandoc
+    pkgs.verilator
+    pkgs.verilog
+    pkgs.gtkwave
+    pkgs.tcl
+    pkgs.gperf
+    pkgs.autoconf
+    pkgs.zlib
+    pkgs.gmp
+    pkgs.screen
+    pkgs.minicom
+    pkgs.bluespec
+  ];
+  XRAY_VIVADO_SETTINGS = "/home/adam/opt/Xilinx/Vivado/2017.2/settings64.sh";
+}
